f3a958f6bf5b72ac22f84de14e175e324fadeabf
adhrox97 <57478421+adhrox97@users.noreply.github.com>
2022-11-28 22:40:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
     API_VERSION
 } = require("./constants") */
 
-const PORT = process.env.DB_PORT || 3977
+//const PORT = process.env.DB_PORT || 3977
 
 mongoose.connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/`,
@@ -18,11 +18,11 @@ mongoose.connect(
 
         console.log("La conexion con la base de datos ha sido exitosa");
         
-        app.listen(PORT, () => {
+        app.listen(process.env.PORT, () => {
             console.log("####################")
             console.log("##### API REST #####")
             console.log("####################")
-            console.log(`http://${process.env.IP_SERVER}:${PORT}/api/${process.env.API_VERSION}`)
+            console.log(`http://${process.env.IP_SERVER}:${process.env.PORT}/api/${process.env.API_VERSION}`)
         })
     }
     )
